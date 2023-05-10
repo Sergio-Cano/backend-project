d7b4af68c33b71c8757b1e549c7f8b78e6feb9cf
Sergio-Cano <sergio95cano@hotmail.com>
2023-05-10 21:00:59 +0200

    Added logout function

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const authCtrl = require("../../controllers/auth");
+const authorizer = require("../../middlewares/authorizer");
 
 module.exports = (db) => {
     router.post("/register", authCtrl.register(db));
     router.post("/login", authCtrl.login(db));
+    router.post("/logout", authorizer, authCtrl.logout());
 
     return router;
 }
